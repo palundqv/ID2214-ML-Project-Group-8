52c0c8244319be1fb26882d445f44a5b57ba896a
Gustav Axelsson <gustavax@kth.se>
2023-12-13 00:46:57 +0100

    Added features to evaluating features
    Mainly bugfixing for smote

--- evaluating_features.py
+++ evaluating_features.py
@@ -12,6 +12,7 @@ import DescriptionFeaturesSelection as dsf
 import DataPreprocessing as dprep
 import HyperparamaterTuning.grid_search as hyperGrid
 import HyperparamaterTuning.random_search as hyperRandom
+import datetime
 
 from imblearn.over_sampling import SMOTE
 
@@ -58,8 +59,8 @@ def new_balance_labels_down(X,y):
     df = pd.concat([pd.DataFrame(X), pd.Series(y, name='label')], axis=1)
 
     original_counts = df['label'].value_counts()
-    print("Original Label Counts:")
-    print(original_counts)
+    #print("Original Label Counts:")
+    #print(original_counts)
 
     minority_label = original_counts.idxmin()
     majority_label = original_counts.idxmax()
@@ -67,7 +68,7 @@ def new_balance_labels_down(X,y):
     label_difference = original_counts[majority_label] - original_counts[minority_label]
 
     if label_difference == 0:
-        print("Labels are already balanced.")
+        #print("Labels are already balanced.")
         return X, y
 
     majority_indices = df[df['label'] == majority_label].index
@@ -76,9 +77,9 @@ def new_balance_labels_down(X,y):
     balanced_df = df.drop(indices_to_remove)
 
     new_counts = balanced_df['label'].value_counts()
-    print("\nNew Label Counts:")
-    print(new_counts)
-    return balanced_df.drop('label', axis=1).values, balanced_df['label'].values
+    #print("\nNew Label Counts:")
+    #print(new_counts)
+    return balanced_df.drop('label', axis=1), balanced_df['label']
 
 def test_for_hyperparameters_grid(training, training_lables):
     n_kbest_features_to_test = [10,30,50,80,100]
@@ -113,7 +114,7 @@ def ClassifierWithFeatures(training, training_lables,numberOfFeatures, print_res
     
 def manual_crossValidation(training, training_lables, number_of_features = 50, use_classifier="randomForest", sampling="smote"):
     if use_classifier == "randomForest":
-        classifier = RandomForestClassifier(bootstrap=True, max_depth=40, min_samples_leaf=2,min_samples_split=2,n_estimators=200,class_weight="balanced",random_state=42)
+        classifier = RandomForestClassifier(n_estimators=200,random_state=42)
     elif use_classifier == "MLP":
         classifier = MLPClassifier(activation = 'relu',alpha= 0.0001, hidden_layer_sizes=(50,))
     else:
@@ -124,7 +125,7 @@ def manual_crossValidation(training, training_lables, number_of_features = 50, u
     y = training_lables.copy()
 
     # Set up cross-validation
-    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    kf = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
 
     accuracies = []
     auc_scores = []
@@ -134,19 +135,24 @@ def manual_crossValidation(training, training_lables, number_of_features = 50, u
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-        n_best_features = dsf.select_n_best_features_selectKBest(X_train, y_train, n_features=number_of_features) ## should be moved into the fold calculation loop
-        X_train = X_train[list(n_best_features.keys())]
-        X_test = X_test[list(n_best_features.keys())]
-
+        #n_best_features = dsf.select_n_best_features_selectKBest(X_train, y_train, n_features=number_of_features) ## should be moved into the fold calculation loop
+        #X_train = X_train[list(n_best_features.keys())]
+        #X_test = X_test[list(n_best_features.keys())]
 
         if sampling=="smote":
             # Apply SMOTE to the training set
             smote = SMOTE(sampling_strategy='auto', random_state=42)
             X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
+            X_train_resampled = pd.DataFrame(X_train_resampled, columns=X_train.columns)
         elif sampling=="under":
             X_train_resampled, y_train_resampled = new_balance_labels_down(X_train, y_train)
+            #X_train_resampled = pd.DataFrame(X_train_resampled, columns=X_train.columns)
         else:
             X_train_resampled, y_train_resampled = X_train, y_train
+
+        n_best_features = dsf.select_n_best_features_selectKBest(X_train_resampled, y_train_resampled, n_features=number_of_features) 
+        X_train_resampled = X_train_resampled[list(n_best_features.keys())]
+        X_test = X_test[list(n_best_features.keys())]
 
         classifier.fit(X_train_resampled, y_train_resampled)
         y_pred = classifier.predict(X_test)
@@ -179,55 +185,54 @@ def test_random_forest_smote(training_data):
 
     smote = SMOTE(sampling_strategy='auto', random_state=42)
     X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
-    n_best_features = dsf.select_n_best_features_selectKBest(X_train_resampled, y_train_resampled, n_features=50)
+    X_train_resampled_df = pd.DataFrame(X_train_resampled, columns=X.columns)
 
-    classifier.fit(X_train_resampled[list(n_best_features.keys())], y_train_resampled)
+    n_best_features = dsf.select_n_best_features_selectKBest(X_train_resampled_df, y_train_resampled, n_features=50)
+
+    classifier.fit(X_train_resampled_df[list(n_best_features.keys())], y_train_resampled)
 
     y_pred = classifier.predict(X_test[list(n_best_features.keys())])
 
     accuracy = accuracy_score(y_test, y_pred)
-    auc_score = roc_auc_score(y_test, classifier.predict_proba(X_test)[:, 1])
+    auc_score = roc_auc_score(y_test, classifier.predict_proba(X_test[list(n_best_features.keys())])[:, 1])
     cm = confusion_matrix(y_test, y_pred)
 
     print("Accuracy: ",accuracy)
     print("auc_score: ",auc_score)
-
-
-
-
-
-
-
-    
-
+    print(cm)
 
 
 if __name__ == "__main__":
     training_data_path = 'csvData\\training_merged_fingerprints207.csv'
     training_data = pd.read_csv(training_data_path)
 
-
     training_data, column_filter = dprep.create_column_filter(training_data)
     training_data, imputation = dprep.create_imputation(training_data)
 
+    # test_random_forest_smote(training_data)
+
     training = training_data.drop(columns = ["INDEX","ACTIVE"])
     training_lables = training_data["ACTIVE"]
-
-    #smote = SMOTE(sampling_strategy='auto', random_state=42)
-    #training_resampled, training_lables_resampled = smote.fit_resample(training, training_lables)
     
     ## Testing Random forest with and without smote
+    print("Starting random forest oversampling")
     results_oversampled = manual_crossValidation(training, training_lables, 50,use_classifier='randomForest',sampling="smote")
+    print("starting random forest undersampling")
     results_undersampled = manual_crossValidation(training, training_lables, 50,use_classifier='randomForest',sampling="under")
 
     print("Random-Forest with SMOTE: ", results_oversampled["average_auc_score"])
+    print(results_oversampled["confusion_matrix"])
     print("Random-Forest no SMOTE: ", results_undersampled["average_auc_score"])
+    print(results_undersampled["confusion_matrix"])
 
     ## Testing MLP with and without smote
-    MLP_results_oversampled = manual_crossValidation(training, training_lables, 50,use_classifier='MLP', sampling="smote")
     MLP_results_undersampled = manual_crossValidation(training, training_lables, 50, use_classifier='MLP',sampling="under")
+    MLP_results_oversampled = manual_crossValidation(training, training_lables, 50,use_classifier='MLP', sampling="smote")
     print("MLP with SMOTE: ", MLP_results_oversampled["average_auc_score"])
+    print(MLP_results_oversampled["confusion_matrix"])
     print("MLP no SMOTE: ", MLP_results_undersampled["average_auc_score"])
+    print(MLP_results_undersampled["confusion_matrix"])
+
 
 
     #test_for_hyperparameters(training, training_lables)
